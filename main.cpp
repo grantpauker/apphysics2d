ed9263d6c106b01af6335efac38c6db1addf3505
ruwix <grantpauker@gmail.com>
2018-02-24 15:30:05 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System/Time.hpp>
 #include <ctime>
+#include <cmath>
 #include <chrono>
 #include <Box2D/Box2D.h>
 #include <apphysics2d/apphysics2d.hpp>
@@ -20,39 +21,38 @@ int main()
 	srand(time(NULL));
 	Viewport2D viewport(VectorU2D(WIDTH, HEIGHT), VectorF2D(0, 9.8), 60);
 
-	Rectangle2D dog(viewport.world, dynamic2D, VectorF2D(300, 100), VectorF2D(20, 20), 1.f, 0, 0.4f);
-	dog.setOrigin(dog.getSize().x / 2, dog.getSize().y / 2);
+	Rectangle2D dog(viewport.world, dynamic2D, VectorF2D(300, 100), VectorF2D(100, 100), 1.f, 0, 0.4f);
+	dog.setOrigin(VectorF2D(dog.getSize().x / 2, dog.getSize().y / 2));
 	dog.setVisualProperties(Color2D(255, 0, 0, 128), Color2D(255, 0, 0), 1);
 	viewport.addPhysicsBody(&dog);
 
 	Rectangle2D ground(viewport.world, static2D, VectorF2D(WIDTH / 2, HEIGHT), VectorF2D(WIDTH, 50), 1.f, 0.5f, 0.4f);
-	ground.setOrigin(ground.getSize().x / 2, ground.getSize().y / 2);
+	ground.setOrigin(VectorF2D(ground.getSize().x / 2, ground.getSize().y / 2));
 	ground.setVisualProperties(Color2D(255, 0, 0, 128), Color2D(255, 0, 0), 1);
 	viewport.addPhysicsBody(&ground);
 
 	Rectangle2D wall_left(viewport.world, static2D, VectorF2D(0, HEIGHT / 2), VectorF2D(50, HEIGHT), 1.f, 0.5f, 0.4f);
-	wall_left.setOrigin(wall_left.getSize().x / 2, wall_left.getSize().y / 2);
+	wall_left.setOrigin(VectorF2D(wall_left.getSize().x / 2, wall_left.getSize().y / 2));
 	wall_left.setVisualProperties(Color2D(255, 0, 0, 128), Color2D(255, 0, 0), 1);
 	viewport.addPhysicsBody(&wall_left);
 
 	Rectangle2D wall_right(viewport.world, static2D, VectorF2D(WIDTH, HEIGHT / 2), VectorF2D(50, HEIGHT), 1.f, 0.5f, 0.4f);
-	wall_right.setOrigin(wall_right.getSize().x / 2, wall_right.getSize().y / 2);
+	wall_right.setOrigin(VectorF2D(wall_right.getSize().x / 2, wall_right.getSize().y / 2));
 	wall_right.setVisualProperties(Color2D(255, 0, 0, 128), Color2D(255, 0, 0), 1);
 	viewport.addPhysicsBody(&wall_right);
 
 	sf::Clock clock;
+	float start_time = 0;
+	int frame_count = 0;
 
 	while (viewport.window.isOpen())
 	{
-		sf::Time time = clock.getElapsedTime();
-		float seconds = time.asSeconds();
+		start_time = clock.getElapsedTime().asSeconds();
 
-		if (seconds > 0.9 && seconds < 1.1)
+		if (frame_count == 60)
 		{
-			// dog.getBody()->SetLinearVelocity(b2Vec2(10.f, dog.getBody()->GetLinearVelocity().y));
-			// dog.setSize(200, 200);
-			dog.getBody()->ApplyForce(b2Vec2(100.f, 0.f), b2Vec2(0.f, 0.f), true);
-			// cout << "hey\n";
+			cout << "start: " << dog.getTransform().pos.x << endl;
+			dog.addTransform(VectorF2D(WIDTH / 2 / 30, 0), 0);
 		}
 		// cout << dog.getOrigin().x << endl;
 		sf::Event event;
@@ -60,6 +60,9 @@ int main()
 		{
 			switch (event.type)
 			{
+			case sf::Event::Resized:
+				viewport.window.setView(sf::View(sf::FloatRect(0, 0, event.size.width, event.size.height)));
+				break;
 			case sf::Event::Closed:
 				viewport.window.close();
 				break;
@@ -87,13 +90,13 @@ int main()
 		viewport.draw(&wall_right);
 
 		viewport.window.display();
-		START();
-		if (dog.isCollision())
-		{
-			cout << dog.getSize().x << endl;
-			dog.setSize(dog.getSize().x+5.f,dog.getSize().y+1.f);
-		}
-		// END();
+		// if (dog.isCollision())
+		// {
+		// 	cout << dog.getSize().x << endl;
+		// 	dog.setSize(dog.getSize().x + 5.f, dog.getSize().y + 1.f);
+		// }
+		frame_count++;
+		cout << "end: " << dog.getTransform().pos.x << endl;
 	}
 	return EXIT_SUCCESS;
 }
